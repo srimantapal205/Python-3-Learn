426530cfdbae73d963616e8edfa7466c58bdd4c4
Srimanta Pal <srimantapal205@hotmail.com>
2025-01-09 23:11:05 +0530

    Bulit in function

---
+++ PythonBasic/builtinfunction_methods.py
@@ -0,0 +1,39 @@
+'''
+str() - converts any data type into a string
+int() - converts any data type into an integer
+type() - returns the data type of the argument
+print() - prints to the console
+len() - returns the length of an item
+
+'''
+
+print(len('Now this idea of immutability is something '))
+
+str1 = "Now this idea of immutability is something we'll explore more and more"
+print(str1.split(' '))
+print(str1.split(' ')[::-1])
+print(len(str1.split(' ')[::-1]))
+
+s = str1.split(' ')[::-1]
+
+for i in s:
+    print(i)
+
+ 
+
+lgs = max(str1.split(' '), key=len)
+print(f'{lgs} - {len(lgs) }')
+
+print(f'{lgs.upper()} - {len(lgs) }')
+print(f'{lgs.capitalize()} - {len(lgs) }')
+print(f'{lgs.lower()} - {len(lgs) }')
+
+print(f'{lgs.find('of')} - {len(lgs) }')
+
+
+
+
+mings = min(str1.split(' '), key=len)
+print(f'{mings} - {len(mings) }')
+
+
