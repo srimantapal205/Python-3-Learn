7d39aec20fd63a8ad80365de8333d67a2933d0a7
Srimanta Pal <srimantapal205@hotmail.com>
2025-01-10 21:28:16 +0530

    Learn list

---
+++ PythonBasic/lists_in_python.py
@@ -0,0 +1,21 @@
+lst1 = [1,2,3,4,5,6]
+lst2 = ['a','b','c','d','e','f']
+lst3 = [1,2,'a',True,3.5]
+
+
+#List Slicing
+
+cart = ['apple', 'banana', 'cherry', 'date', 'eggfruit', 'fig', 'grape']
+
+print(cart[0])
+print(cart[0:2:2])
+print(cart[0::2])
+print(cart[0:2:2])
+print(cart[::-1])
+print(cart[-1:2:-2])
+
+cart[0] = 'apricot'
+print(cart)
+
+
+
