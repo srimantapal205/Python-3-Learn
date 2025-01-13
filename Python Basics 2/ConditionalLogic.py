b23263e9a261fb1846c855ea0431dd7986568d48
Srimanta Pal <srimantapal205@hotmail.com>
2025-01-13 06:57:07 +0530

    Update file

--- Python Basics 2/ConditionalLogic.py
+++ Python Basics 2/ConditionalLogic.py
@@ -65,3 +65,35 @@ isFriend = True
 if isUser or isFriend:
     print("Best Friends Forever")
     
+
+#Logical Operators
+# and, or, not, >, <, >=, <=, ==, !=
+print(0 and 1)#
+print(0 or 1)#
+print(not 0)#
+print(not 1)#
+print(0 > 1)#
+print(0 < 1)#
+print(0 >= 1)#
+print(0 <= 1)#
+print(0 == 1)#
+print(0 != 1)#
+print(0 == 0)#False
+print(0 != 0)#True
+print(0 is 0) #True
+
+
+print('a' > 'A')
+#why 'a' > 'A' is true?
+#ASCII values of 'a' and 'A' are 97 and 65 respectively.
+print('Coditional Value')
+print(1<2<3<4)
+print(1>2<3>4)
+print(1>=2<=3>=4)
+print(1<=2>=3<=4)
+
+#NOT operator 
+#It is used to reverse the logical state of its operand and return oposite value.
+print('NOT operator')
+print(not(False))
+print(not(True))
