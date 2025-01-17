2d1dc9a6d90ec3cdd8bb13954d7bb82f983764e4
Srimanta Pal <srimantapal205@hotmail.com>
2025-01-17 06:04:30 +0530

    Update function

--- Python Basics 2/functions.py
+++ Python Basics 2/functions.py
@@ -26,11 +26,66 @@ Types of Arguments:
     Variable-Length Arguments: Accept arbitrary numbers of arguments using *args or **kwargs.
 
 '''
+
+
 def say_hey(name, emoji):
     print(f'Hello {name}, {emoji}')
 
 
-#Argument
+#Positional Argument
 say_hey('dff', ':)') #Call or invocking the function
+
+#Keyword Arguments 
+
+say_hey(emoji=':)', name='Baby')
+
+
+# Default Parameter and Argument
+
+def say_hey_v2(name ='xyz abc', emoji=':)'):
+    print(f'Hello {name}, {emoji}')
+    
+say_hey_v2()
+say_hey_v2('wer qwe', ':)')
+say_hey_v2('Timmy')
+
+
+# Return function
+'''
+In Python, a function with a return statement provides a way to send a result or value back to the caller. The return statement ends the function's execution and specifies what value should be returned to the function's caller.
+
+    Returning a Single Value: The function returns one value to the caller.
+    Returning Multiple Values: Python allows functions to return multiple values as a tuple.
+    No return Statement: If a function doesn't have a return statement, it implicitly returns
+
+def function_name(parameters):
+    # Function logic
+    return value
+
+
+'''
+def sum(num1, num2):
+    num1 + num2
+
+sum(4,6) # Nothing will print
+
+print(sum(4,6)) #None
+
+def sum_return(num1, num2):
+    return num1 + num2
+
+print(sum_return(4,6))
+
+total = sum_return(50, 61)
+print(sum_return(100, total))
+
+#Function within in a function
+
+
+def fun(n1, n2):
+    def fun_v2(n1, n2):
+        return n1+n2
+    return(fun_v2(n1,n2))
+print(fun(5,4))
 
 
