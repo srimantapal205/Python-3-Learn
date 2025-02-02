a748c7d455f3d1f176041457ffb5b2a5ce4131d7
Srimanta Pal <srimantapal205@hotmail.com>
2025-02-02 05:49:35 +0530

    Add debug process file

---
+++ DebugCode/debuging.py
@@ -0,0 +1,29 @@
+#Debugging
+#linting
+#Ide/Edior
+#Read error
+
+import pdb
+def addition(num1, num2):
+    pdb.set_trace()
+    return num1+num2
+
+print(addition(11,'fdsf'))
+
+
+'''
+Documented commands (type help <topic>):
+========================================
+EOF    c          d        h         list      q        rv       undisplay
+a      cl         debug    help      ll        quit     s        unt      
+alias  clear      disable  ignore    longlist  r        source   until    
+args   commands   display  interact  n         restart  step     up       
+b      condition  down     j         next      return   tbreak   w        
+break  cont       enable   jump      p         retval   u        whatis   
+bt     continue   exit     l         pp        run      unalias  where    
+
+Miscellaneous help topics:
+==========================
+exec  pdb
+
+'''
